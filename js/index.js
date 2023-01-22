026b9f5482ac33d169bd5ab871330997b84ec1ca
Laura Ramírez <ana_p.y.t@hotmail.com>
2023-01-22 14:10:23 -0600

    Añadiendo nueva sección

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 document.oncontextmenu = function(){return false}
+
+
+function cambiarElemento(){
+    document.getElementById("Adal").style.display = "none";
+    }
+
+
+
+function mostrarAdal(){
+        document.getElementById("Irwin").style.display = "none";
+        document.getElementById("Ramses").style.display = "none";
+        document.getElementById("Adal").style.display = "block";
+        }
+
+function mostrarIrwin(){
+            document.getElementById("Adal").style.display = "none";
+            document.getElementById("Ramses").style.display = "none";
+            document.getElementById("Irwin").style.display = "block";
+        }
+    function mostrarRamses(){
+            document.getElementById("Adal").style.display = "none";
+            document.getElementById("Ramses").style.display = "block";
+            document.getElementById("Irwin").style.display = "none";
+        }
